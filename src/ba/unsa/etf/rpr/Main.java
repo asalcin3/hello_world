5ae09a1abfc18c074f590db4493ec73409a69b79
Adna Salcin <asalcin3@etf.unsa.ba>
2020-10-15 21:25:45 +0200

    promjenjen tekst na ekranu!

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-      System.out.println("Pozdrav gledaocima videa!");
+      System.out.println("Promjena 3!");
     }
 }
